cd6969cff3ccd2f0687360e48559ea2644dff714
Roman <Roman.Mettler@gmail.com>
2022-06-11 13:04:21 -0300

    Agrega morgan como logger y cors al proyecto

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import container from './config/inversify';
 import { init as initUserModule } from './module/user/module'
 import { StatusCodes } from "http-status-codes";
+import cors from 'cors'
+import morgan from 'morgan'
 
 const app = express()
 app.use(express.urlencoded({
@@ -17,13 +19,17 @@ initUserModule(app, container)
 
 const PORT = process.env.PORT || 8000
 
+app.use(cors({credentials: true, origin: <string>process.env.ORIGIN_API_CONSUMER}))
+app.use(morgan('dev'))
+
+//si encuentra algun error imprevisto
 app.use((err: Error, req: Request, res: Response, next: NextFunction): void => {
   console.log(err.stack)
   res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ error: err.message })
 })
 
+//si no encuentre ruta
 app.use(function (req: Request, res: Response, next: NextFunction) {
-
   const method = req.method
   const route = req.originalUrl
   res.status(404).send({
