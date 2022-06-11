153caac33c4a57631112678d46a7f0256711f54f
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2022-06-11 03:38:42 -0300

    Agrega nuevo tema con los colores del la app

---
+++ client/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#002E25',
+    },
+    secondary: {
+      main: '#008060',
+    },
+  },
+});
+
+export default theme;
