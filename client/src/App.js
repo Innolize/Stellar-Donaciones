ac22b432b0bbcfd1d72a577374ca40496043428e
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2022-06-11 03:38:09 -0300

    Actualiza providers

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import AppRoutes from './routes/AppRoutes';
+import UserProvider from './context/UserProvider';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <AppRoutes />
+        </ThemeProvider>
+      </UserProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
