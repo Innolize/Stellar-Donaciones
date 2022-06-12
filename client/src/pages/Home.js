06719d750296d7cae629db1bbb2c055d30006c20
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2022-06-12 19:23:30 -0300

    Agrega breakpoints para mobile

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,18 +2,32 @@ import { Box, Button, Divider, Grid } from '@mui/material';
 import { Fragment, useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import ErrorMessage from '../components/ErrorMessage';
-import DonationModal from '../components/DonationModal';
+import TransactionModal from '../components/TransactionModal';
 import LoadingAnimation from '../components/LoadingAnimation';
 import ProyectCard from '../components/ProyectCard';
 import useGetProyects from '../hooks/useGetProyects';
 import { UserContext } from '../context/UserProvider';
 import useDonate from '../hooks/useDonate';
+import * as yup from 'yup';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  btn: {
+    [theme.breakpoints.up('xs')]: {
+      marginBottom: '50px',
+    },
+    [theme.breakpoints.up('sm')]: {
+      marginTop: '220px',
+    },
+  },
+}));
 
 const Home = () => {
+  const classes = useStyles();
   const getProyects = useGetProyects();
   const { user } = useContext(UserContext);
   const navigate = useNavigate();
-  const [proyectId, setProyectId] = useState(false);
+  const [proyect, setProyect] = useState(false);
   const [open, setOpen] = useState(false);
   const donate = useDonate();
 
@@ -25,8 +39,8 @@ const Home = () => {
     setOpen(false);
   };
 
-  const handleClickDonate = (proyectId) => {
-    setProyectId(proyectId);
+  const handleClickDonate = (proyect) => {
+    setProyect(proyect);
     if (!user) {
       navigate('/login');
     } else {
@@ -35,7 +49,7 @@ const Home = () => {
   };
 
   const handleDonation = (amount) => {
-    donate.mutate({ proyectId, amount, userId: user?.id });
+    donate.mutate({ proyectId: proyect.id, amount });
   };
 
   if (getProyects.isLoading) {
@@ -46,37 +60,49 @@ const Home = () => {
     return <ErrorMessage error="We could not load this page" />;
   }
 
+  const validationSchema = yup.object({
+    amount: yup
+      .number()
+      .required('Enter an amount')
+      .positive('The amount must be greater than 0')
+      .max(user.balance, 'The amount cannot be greater than your balance'),
+  });
+
   return (
-    <Grid container spacing={2}>
-      <DonationModal
+    <Grid container spacing={{ xs: 1, sm: 2 }}>
+      <TransactionModal
+        validationSchema={validationSchema}
         isLoading={donate.isLoading}
+        title={'Make a donation to: ' + proyect.name}
+        text={'You have ' + user.balance + ' XML in your wallet. How much do you want to donate?'}
+        isError={donate.isError}
         open={open}
         handleClose={handleClose}
         submit={handleDonation}
-      ></DonationModal>
-      {getProyects.isSuccess &&
-        getProyects.data.map((proyect) => (
-          <Fragment key={proyect.id}>
-            <Grid item xs={3}>
-              <Box mt={23} display="flex" justifyContent="center">
-                <Button
-                  size="large"
-                  color="secondary"
-                  variant="contained"
-                  onClick={() => handleClickDonate(proyect.id)}
-                >
-                  Donate
-                </Button>
-              </Box>
-            </Grid>
-            <Grid item xs={9}>
-              <Link style={{ textDecoration: 'none' }} to={'/proyects/' + proyect.id}>
-                <ProyectCard proyect={proyect} actionArea />
-              </Link>
-            </Grid>
-            <Divider sx={{ width: '100%' }}></Divider>
-          </Fragment>
-        ))}
+      ></TransactionModal>
+      {getProyects.data.map((proyect) => (
+        <Fragment key={proyect.id}>
+          <Grid item xs={12} sm={9}>
+            <Link style={{ textDecoration: 'none' }} to={'/proyects/' + proyect.id}>
+              <ProyectCard proyect={proyect} actionArea />
+            </Link>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <Box display="flex" justifyContent="center">
+              <Button
+                className={classes.btn}
+                size="large"
+                color="secondary"
+                variant="contained"
+                onClick={() => handleClickDonate(proyect)}
+              >
+                Donate
+              </Button>
+            </Box>
+          </Grid>
+          <Divider sx={{ width: '100%' }}></Divider>
+        </Fragment>
+      ))}
     </Grid>
   );
 };
