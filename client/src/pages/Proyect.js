b03baead9fb1d2a58c7eb1e73b2ada67c1e090e4
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2022-06-12 04:46:08 -0300

    Agrega pagina de proyecto individual

--- client/src/pages/Proyect.js
+++ client/src/pages/Proyect.js
@@ -1,9 +1,34 @@
-import { useParams } from 'react-router-dom';
+import { Box, Typography, Link } from '@mui/material';
+import { Link as RouterLink, useParams } from 'react-router-dom';
+import ErrorMessage from '../components/ErrorMessage';
+import LoadingAnimation from '../components/LoadingAnimation';
+import ProyectCard from '../components/ProyectCard';
+import useGetOrganization from '../hooks/useGetOrganization';
+import useGetProyect from '../hooks/useGetProyect';
 
 const Proyect = () => {
   const { id } = useParams();
+  const getProyect = useGetProyect(id);
+  const getOrganization = useGetOrganization(getProyect.data?.author_id);
 
-  return <div>Proyect: {id}</div>;
+  if (getProyect.isLoading || getOrganization.isLoading) {
+    return <LoadingAnimation />;
+  }
+
+  if (getProyect.isError || getOrganization.isError) {
+    return <ErrorMessage error="We could not load this page" />;
+  }
+
+  return (
+    <Box m={2}>
+      <Box ml={4}>
+        <Link component={RouterLink} to={'/organizations/' + getOrganization.data.id}>
+          <Typography variant="h5">Organization: {getOrganization.data.email}</Typography>
+        </Link>
+      </Box>
+      <ProyectCard variant="detailed" proyect={getProyect.data} />
+    </Box>
+  );
 };
 
 export default Proyect;
