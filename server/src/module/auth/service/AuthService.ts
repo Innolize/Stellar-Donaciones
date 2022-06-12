3444d188335d39ccb2e87b04e69fda4d23ef68fd
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2022-06-12 12:23:43 -0300

    Agrega ruta para verificar jwt token El cliente ahora puede acceder a los datos del usuario solo con el token

--- server/src/module/auth/service/AuthService.ts
+++ server/src/module/auth/service/AuthService.ts
@@ -1,7 +1,8 @@
 import { inject, injectable } from "inversify"
-import { sign } from "jsonwebtoken"
+import { sign,verify } from "jsonwebtoken"
 import { TYPES } from "../../../config/inversify.types"
 import { IUserRepository } from "../../user/interface/IUserRepository"
+import { IJwtToken } from "../interface/IJwtToken"
 import { ILoginResponse } from "../interface/ILoginResponse"
 
 @injectable()
@@ -12,7 +13,6 @@ export class AuthService {
         this.userRepository = userRepository
     }
     async login(userId: number): Promise<ILoginResponse> {
-        console.log(userId)
         const user = await this.userRepository.findUserById(userId)
         if (!user) {
             throw Error("Usuario inexistente")
@@ -23,6 +23,23 @@ export class AuthService {
         return {
             user: rest,
             access_token
+        }
+    }
+
+    async verifyToken(userToken: string) {
+        const token = verify(userToken, <string>process.env.JWT_SECRET) as unknown as IJwtToken
+        const { sub } = token
+        const payload = { sub }
+        const user = await this.userRepository.findUserById(sub)
+        if (!user) {
+            throw Error("Usuario inexistente")
+        }
+        const { kPrivate, password, ...rest } = user
+
+        const access_token = this.signAccessToken(payload)
+        return {
+            user: rest,
+            access_token,
         }
     }
 
