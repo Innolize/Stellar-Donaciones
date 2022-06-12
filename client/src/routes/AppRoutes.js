55bd06afa735b167f35d54cc274ca79398d79870
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2022-06-12 17:18:15 -0300

    Agrega componente de ruta auth

--- client/src/routes/AppRoutes.js
+++ client/src/routes/AppRoutes.js
@@ -1,11 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useContext } from 'react';
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import Layout from '../components/Layout';
+import { UserContext } from '../context/UserProvider';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Organization from '../pages/Organization';
 import Profile from '../pages/Profile';
 import Proyect from '../pages/Proyect';
 import SignUp from '../pages/SignUp';
+
+function RequireAuth() {
+  let { user } = useContext(UserContext);
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+}
 
 const AppRoutes = () => {
   return (
@@ -15,7 +27,9 @@ const AppRoutes = () => {
           <Route index element={<Home />}></Route>
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<SignUp />} />
-          <Route path="profile" element={<Profile />} />
+          <Route element={<RequireAuth />}>
+            <Route path="profile" element={<Profile />} />
+          </Route>
           <Route path="organizations/:id" element={<Organization />} />
           <Route path="proyects/:id" element={<Proyect />}></Route>
         </Route>
