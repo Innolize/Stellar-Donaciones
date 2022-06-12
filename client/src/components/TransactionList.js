0c922a36d939aff8dbfba98d3077b7cbbd4cd6ff
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2022-06-12 17:23:49 -0300

    Arregla bug en el filtro de pagos

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,23 +1,23 @@
 import { Box, Divider, List, ListItem, ListItemText, Typography } from '@mui/material';
 
-const TransactionList = (payments) => {
+const TransactionList = ({ payments }) => {
   return (
     <Box>
       <Typography align="center" variant="h3">
         Transactions
       </Typography>
       <List sx={{ width: '100%', maxWidth: 800, bgcolor: 'background.paper', overflow: 'auto' }}>
-        <Typography variant="h4">Payments</Typography>
+        <Typography variant="h4">Latest payments</Typography>
         <Divider />
         {payments
-          .filter((payment) => payment.type === 'payment')
+          .filter((payment) => payment.type === 'payment' && payment.asset_type !== 'credit_alphanum4')
           .map((payment) => (
             <Box key={payment.id}>
               <ListItem alignItems="flex-start">
                 <ListItemText>
                   <Box m={1}>
                     <Typography variant="subtitle2" color="text.primary">
-                      From (your wallet): {payment.from}
+                      From : {payment.from}
                     </Typography>
                   </Box>
 
