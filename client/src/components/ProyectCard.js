fbb3d0ad36796cec62c20a0e71a7dd38332c199c
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2022-06-16 16:34:11 -0300

    Remueve funciones de proyect card No van a ser necesarias para el componente

--- client/src/components/ProyectCard.js
+++ client/src/components/ProyectCard.js
@@ -1,41 +1,7 @@
-import { Box, Card, CardActionArea, CardMedia, CardContent, Typography, Divider, Grid } from '@mui/material';
+import { Box, Card, CardActionArea, CardMedia, CardContent, Typography, Divider } from '@mui/material';
 import LinearProgressBar from './LinearProgressBar';
 
 const ProyectCard = ({ proyect, variant, actionArea }) => {
-  const timeToFinish = (isoString) => {
-    const today = new Date();
-    const MS_PER_MIN = 60000;
-    let minDiff = Math.floor((new Date(isoString).getTime() - today) / MS_PER_MIN);
-    let hourDiff = 0;
-    let dayDiff = 0;
-
-    while (minDiff > 60) {
-      minDiff -= 60;
-      hourDiff++;
-    }
-
-    while (hourDiff > 24) {
-      hourDiff -= 24;
-      dayDiff++;
-    }
-
-    if (dayDiff <= 0 && hourDiff <= 0 && minDiff <= 0) {
-      return 'Already ended';
-    }
-
-    return `${dayDiff} days, ${hourDiff} hours and ${minDiff} minutes left. `;
-  };
-
-  const toDate = (isoString) => {
-    const date = new Date(isoString).toLocaleDateString('en-GB');
-    return date;
-  };
-
-  const getHoursSecondsMinutes = (isoString) => {
-    const date = new Date(isoString);
-    return date.toLocaleString('en-US', { minute: 'numeric', hour: 'numeric', hour12: true });
-  };
-
   // You need to pass the actionArea(Boolean) prop to get click effects
   const CardArea = actionArea ? CardActionArea : Box;
 
@@ -44,7 +10,13 @@ const ProyectCard = ({ proyect, variant, actionArea }) => {
       <CardArea>
         <CardMedia component="img" height="140" image={proyect.image} />
         <CardContent>
-          <Typography align="center" gutterBottom variant="h5" component="div">
+          <Typography
+            sx={{ overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
+            align="center"
+            gutterBottom
+            variant="h5"
+            component="div"
+          >
             {proyect.name}
           </Typography>
           <Divider />
@@ -63,28 +35,6 @@ const ProyectCard = ({ proyect, variant, actionArea }) => {
             </Box>
           </Box>
         </CardContent>
-        {variant === 'detailed' && (
-          <Box m={5}>
-            <Grid container spacing={2}>
-              <Grid item xs={6}>
-                <Typography variant="h6" align="center">
-                  Started the day: {toDate(proyect.start)} at {getHoursSecondsMinutes(proyect.start)}
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="h6" align="center">
-                  Finishes the day: {toDate(proyect.to)} at {getHoursSecondsMinutes(proyect.to)}
-                </Typography>
-              </Grid>
-            </Grid>
-
-            <Box mt={4}>
-              <Typography align="center" variant="h6" color="darkgreen">
-                Time left: {timeToFinish(proyect.to)}
-              </Typography>
-            </Box>
-          </Box>
-        )}
       </CardArea>
     </Card>
   );
