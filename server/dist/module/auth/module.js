2ce832a7520fb581a48f93a8b946872d4f394ac5
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2022-06-16 20:52:13 -0300

    Cambios para deploy a heroku

--- server/dist/module/auth/module.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.init = void 0;
-const inversify_types_1 = require("../../config/inversify.types");
-const init = (app, container) => {
-    const controller = container.get(inversify_types_1.TYPES.Auth.Controller);
-    controller.configureRoutes(app);
-};
-exports.init = init;
--- server/dist/module/auth/util/passportMiddlewares.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwtAuthentication = exports.localAuthentication = void 0;
-const passport_1 = __importDefault(require("passport"));
-exports.localAuthentication = passport_1.default.authenticate('local', { session: false });
-exports.jwtAuthentication = passport_1.default.authenticate('jwt', { session: false });
