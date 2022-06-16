494edc1cb4713f718dd4d74fe46317f779562c7d
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2022-06-15 21:11:28 -0300

    Agrega funcionalidad para paginacion

--- client/src/hooks/useGetProyects.js
+++ client/src/hooks/useGetProyects.js
@@ -2,17 +2,17 @@ import { useQuery } from 'react-query';
 // import api from '../services/api';
 import mockApi from '../services/mockApi';
 
-const useGetProyects = () => {
-  return useQuery('proyects', getProyects, {
+const useGetProyects = (page, limit) => {
+  return useQuery(['proyects', page], () => getProyects(page, limit), {
     onSuccess: () => {},
     retry: 5,
   });
 };
 
-async function getProyects() {
+async function getProyects(page, limit) {
   try {
     // const response = await api.get('/proyects');
-    const response = await mockApi.get('/proyects');
+    const response = await mockApi.get(`/proyects?page=${page}&limit=${limit}`);
     return response.data;
   } catch (e) {
     throw new Error(e);
