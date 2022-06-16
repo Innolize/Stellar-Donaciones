0bc949218f00ab25fb2d0111c7fc3a3b6f2f2cc1
Matias Zabalegui <matias.daniel.zabalegui@gmail.com>
2022-06-16 16:33:37 -0300

    Implementa simple signer para firmar transacciones

--- client/src/hooks/useAddFunds.js
+++ client/src/hooks/useAddFunds.js
@@ -1,6 +1,7 @@
 import { useSnackbar } from 'notistack';
 import { useMutation } from 'react-query';
 import api from '../services/api';
+import * as StellarSdk from 'stellar-sdk';
 
 const useAddFunds = () => {
   const { enqueueSnackbar } = useSnackbar();
@@ -17,5 +18,33 @@ const useAddFunds = () => {
 async function addFunds({ amount }) {
   return await api.post(`api/transaction/fund/${amount}`);
 }
+
+export const createTransactionXDR = async ({ originPublic, destinationPublic, amount }) => {
+  const server = new StellarSdk.Server('https://horizon-testnet.stellar.org/');
+  try {
+    const originAccountKeypar = StellarSdk.Keypair.fromPublicKey(originPublic);
+    const originAccount = await server.loadAccount(originAccountKeypar.publicKey());
+    const transaction = new StellarSdk.TransactionBuilder(originAccount, {
+      fee: StellarSdk.BASE_FEE,
+      networkPassphrase: StellarSdk.Networks.TESTNET,
+      memo: 'test-memo',
+    })
+      .addOperation(
+        StellarSdk.Operation.payment({
+          destination: destinationPublic,
+          asset: StellarSdk.Asset.native(),
+          amount: amount.toString(),
+        })
+      )
+      .addMemo(StellarSdk.Memo.text('Test Transaction'))
+      .setTimeout(180)
+      .build();
+
+    return transaction.toEnvelope().toXDR('base64');
+  } catch (error) {
+    console.log(error);
+    throw Error('stellar error');
+  }
+};
 
 export default useAddFunds;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,5 @@
 import { Box, Grid } from '@mui/material';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import ErrorMessage from '../components/ErrorMessage';
 import LoadingAnimation from '../components/LoadingAnimation';
 import ProfileCard from '../components/ProfileCard';
@@ -9,14 +9,115 @@ import { UserContext } from '../context/UserProvider';
 import useGetBalance from '../hooks/useGetBalance';
 import useGetPayments from '../hooks/useGetPayments';
 import * as yup from 'yup';
-import useAddFunds from '../hooks/useAddFunds';
+import useAddFunds, { createTransactionXDR } from '../hooks/useAddFunds';
+import * as StellarSdk from 'stellar-sdk';
 
 const Profile = () => {
   const { user } = useContext(UserContext);
-  const [open, setOpen] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
   const getBalance = useGetBalance(user.kPublic);
   const getPayments = useGetPayments(user.kPublic);
   const addFunds = useAddFunds();
+  const [amount, setAmount] = useState(20);
+  const [txOriginPublicKey, setTxOriginPublicKey] = useState(null);
+  const [unsignedXdr, setUnsignedXdr] = useState(null);
+  const simpleSignerUrl = 'https://sign-test.plutodao.finance';
+
+  useEffect(() => {
+    window.addEventListener('message', getAccountPublicKey);
+    window.addEventListener('message', signTx);
+
+    async function createTransaction() {
+      if (!!txOriginPublicKey) {
+        setUnsignedXdr(
+          await createTransactionXDR({ originPublic: txOriginPublicKey, destinationPublic: user.kPublic, amount })
+        );
+      }
+    }
+    async function sign() {
+      if (unsignedXdr) {
+        console.log('entre al sign');
+        openSignWindow(unsignedXdr);
+      }
+    }
+
+    createTransaction();
+    sign();
+    console.log(unsignedXdr);
+
+    return () => {
+      window.removeEventListener('message', getAccountPublicKey);
+      window.removeEventListener('message', signTx);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [amount, txOriginPublicKey, unsignedXdr, user]);
+
+  function openConnectWindow(amount) {
+    setAmount(amount);
+    window.open(`${simpleSignerUrl}/connect`, 'Connect_Window', 'width=360, height=450');
+  }
+
+  async function openSignWindow(unsignedXdr) {
+    const signWindow = window.open(
+      `${simpleSignerUrl}/sign?xdr=${unsignedXdr}`,
+      'Sign_Window',
+      'width=360, height=700'
+    );
+    signWindow.postMessage({ unsignedXdr, description: 'test-description' }, simpleSignerUrl);
+
+    return signWindow;
+  }
+
+  const resetFundValues = () => {
+    setUnsignedXdr(null);
+    setTxOriginPublicKey(null);
+    setAmount(0);
+    setOpenModal(false);
+  };
+
+  async function signTx(e) {
+    if (e.origin === simpleSignerUrl && e.data.type === 'onSign' && e.data.page === 'sign') {
+      console.log('ACA QUIERO LLEGAR');
+      const eventMessage = e.data;
+
+      const signedXdr = eventMessage.message.signedXDR;
+      // Validate the XDR, this is just good practice.
+      if (StellarSdk.xdr.TransactionEnvelope.validateXDR(signedXdr, 'base64')) {
+        const server = new StellarSdk.Server('https://horizon-testnet.stellar.org/'); //remember to update this to the correct value
+
+        // Construct the transaction from the signedXDR
+        // see https://stellar.github.io/js-stellar-sdk/TransactionBuilder.html#.fromXDR
+        const transaction = StellarSdk.TransactionBuilder.fromXDR(
+          signedXdr,
+          'Test SDF Network ; September 2015' //remember to update this to the correct value
+        );
+
+        try {
+          const transactionResult = await server.submitTransaction(transaction);
+          console.log(transactionResult);
+          resetFundValues();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+  }
+
+  function getAccountPublicKey(e) {
+    // Reject messages that are not coming from simple signer (tailor this according to your needs)
+    if (e.origin !== `${simpleSignerUrl}`) {
+      return;
+    }
+    console.log('entre getAccountPublicKey');
+    const messageEvent = e.data;
+
+    if (messageEvent.type === 'onConnect') {
+      const publicKey = messageEvent.message.publicKey;
+      // Validate the public key received. This is just good practice.
+      setTxOriginPublicKey(publicKey);
+      console.log('The public key is', publicKey);
+    }
+  }
 
   if (getBalance.isLoading || getPayments.isLoading) {
     return <LoadingAnimation />;
@@ -34,16 +135,12 @@ const Profile = () => {
       .max(600, 'The amount cannot be greater than 600'),
   });
 
-  const handleAddFunds = (amount) => {
-    addFunds.mutate({ amount });
-  };
-
   const handleClickOpen = () => {
-    setOpen(true);
+    setOpenModal(true);
   };
 
   const handleClose = () => {
-    setOpen(false);
+    setOpenModal(false);
   };
 
   return (
@@ -68,9 +165,9 @@ const Profile = () => {
         title={'Add funds to your account'}
         text={'How much do you want to add to your account?'}
         isError={addFunds.isError}
-        open={open}
+        open={openModal}
         handleClose={handleClose}
-        submit={handleAddFunds}
+        submit={openConnectWindow}
       ></TransactionModal>
     </>
   );
